d2ffc7485e6a08ebbcbdc9e8bc656d120a3367bb
manmeet <manmeet3@gmail.com>
2016-03-06 15:22:35 -0800

    automated story completion when all tasks are done

--- controllers/story.py
+++ controllers/story.py
@@ -20,21 +20,40 @@ def new_story():
           redirect(URL('team', 'backlog'))
     elif form.errors:
         response.flash = 'the form is invalid'
+
     return dict(form=form)
 
 def show_story():
   this_story = db.Story(request.args(0,cast=int)) or redirect(URL('index'))
   db.Task.story_id.default = this_story.id
+
   form = SQLFORM(db.Task) if auth.user else 'You need to log in'
   if form.process().accepted:
     new_pts = this_story.story_points + int(form.vars.task_points)
     this_story.update_record(story_points = new_pts)
     response.flash = 'task added'
+
   alltasks = db(db.Task.story_id==this_story.id).select()
+  if alltasks is None:
+    this_story.update_record(completed='True')
+
+  size=len(alltasks)
+  #print size
+  for task in alltasks:
+    if task.status=="Done":
+      size = size - 1
+      #print size
+  if size == 0:
+    #print 'updating'
+    this_story.update_record(completed='True')
+  else:
+    this_story.update_record(completed='False')
+
 
   movestory=SQLFORM(db.Story, this_story, showid=False, fields=['backlogged','sprint_id'])
   if movestory.process().accepted:
      response.flash = 'Story moved'
+
   return dict(story = this_story, tasks=alltasks, form=form, movestory=movestory)
 
 def show_task():
@@ -43,5 +62,6 @@ def show_task():
     form = SQLFORM(db.Task, this_task, showid=False, fields=['status', 'task_points', 'assigned'])
     if form.process().accepted:
         response.flash = 'task changed'
-        redirect(URL('index'))
+        redirect(URL('story', 'show_story', args=request.args(0,cast=int)))
     return dict(task=this_task, story=task_story, form=form)
+
